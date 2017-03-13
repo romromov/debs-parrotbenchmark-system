f1ceaec24d12a9d478f6a9fdee9601918dc66d24
rkaterinenko <rkaterinenko@agtinternational.com>
2017-03-13 16:06:20 +0100

    Minor update

--- src/main/java/romromov/DebsParrotBenchmarkSystemRunner.java
+++ src/main/java/romromov/DebsParrotBenchmarkSystemRunner.java
@@ -1,7 +1,28 @@
 package romromov;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
+ * Entry point for Docker.
+ *
  * @author Roman Katerinenko
  */
 public class DebsParrotBenchmarkSystemRunner {
+    private static final Logger logger = LoggerFactory.getLogger(DebsParrotBenchmarkSystemRunner.class);
+
+    public static void main(String... args) throws Exception {
+        logger.debug("Running...");
+        DebsParrotBenchmarkSystem system = null;
+        try {
+            system = new DebsParrotBenchmarkSystem();
+            system.init();
+            system.run();
+        } finally {
+            if (system != null) {
+                system.close();
+            }
+        }
+        logger.debug("Finished.");
+    }
 }
